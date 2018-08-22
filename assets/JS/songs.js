abc025c5d23a432006dff07eebc1ebe773975850
RisingGeek <bhavaypuri15@gmail.com>
2018-08-23 00:26:08 +0530

    modified loops

--- assets/JS/songs.js
+++ assets/JS/songs.js
@@ -1,10 +1,10 @@
 $(window).on('load',function() {
     var columns = $("#buttons").find(".col-sm-3");
-    for(let i = 0; i < columns.length; i++) {
+    for(let i in columns) {
         $("body").append("<audio type='audio/mp3'></audio>");
     }
 
-    for(let i = 0; i < columns.length; i++) {
+    for(let i in columns) {
         columns.eq(i).append("<img class='img-fluid'>");
         columns.eq(i).append("<button class='fa fa-play-circle'></button>");
         columns.eq(i).append("<div id='progress'><div id='bar'></div></div>");
@@ -17,7 +17,7 @@ $(window).on('load',function() {
         let audio = $("audio");
         let buttons = columns.find("button");
         let images = columns.find("img");
-        for(let i = 0; i < songs.length; i++) {
+        for(let i in songs) {
             audio.eq(i).addClass(songs[i].class);
             audio[i].src=songs[i].src;
             buttons[i].id = songs[i].class;
